3b68a4e306ce65650181849cec94e1eaa2ea64d2
Ahmad Aditya <isisendiriya@gmail.com>
2026-07-04 03:14:36 +0700

    Update: Validation Loop

--- train.py
+++ train.py
@@ -1,23 +1,26 @@
+from model import build_transformer
+from dataset import BilingualDataset, causal_mask
+from config import get_config, get_weights_file_path, latest_weights_file_path
+
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.optim.lr_scheduler import LambdaLR
 
-from dataset import BilingualDataset
-from model import build_transformer
+import warnings
+from tqdm import tqdm
+import os
+from pathlib import Path
 
-from config import get_weights_file_path, get_config
-
+# Huggingface datasets and tokenizers
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
+import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
-
-import warnings
-from tqdm import tqdm
-from pathlib import Path
 
 def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
     sos_idx = tokenizer_tgt.token_to_id('[SOS]')
